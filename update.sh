073e0af66dc1292281aa8a34123ada847feb7b95
Adam Dobrawy <naczelnik@jawnosc.tk>
2016-06-24 09:00:13 +0200

    Update by cron

--- update.sh
+++ update.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-git pull origin master && pip install -r requirements.txt && python3 main.py && git add . && git commit -m "Update by cron" && git push origin master
+git pull origin master 2>/dev/null && pip install -r requirements.txt && python3 main.py && git add . && git commit -m "Update by cron" && git push origin master 2>/dev/null
