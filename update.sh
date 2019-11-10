7985fd0f4d58cb88339b1b287505008593760aab
Adam Dobrawy <ad-m@users.noreply.github.com>
2019-11-10 15:26:33 +0100

    Update update.sh

--- update.sh
+++ update.sh
@@ -4,5 +4,5 @@ git pull origin master 2>/dev/null
 pip install -r requirements.txt
 python3 main.py
 git add .
-git commit -m "Update by cron"
+git commit -m "Update by cron" || echo 'Nothing to commit'
 git push origin master 2>/dev/null
